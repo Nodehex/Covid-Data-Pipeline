b8939f2e9e1b768321072d62c7f4f88548290742
spacebjorn <30187002+Spacebjorn@users.noreply.github.com>
2020-03-22 22:30:52 +0100

    Added some print info and downloading total cases and deaths sepperately

--- download_data.py
+++ download_data.py
@@ -7,13 +7,18 @@ if not os.path.exists(directory):
     os.makedirs(directory)
 
 def download_csv(url, filename):
+    print(f'Downloading {filename}')
     data = requests.get(url)
-
+    data.raise_for_status()
+    print(f'Writing {filename} to file')
     with open(f'{directory}/{filename}.csv', 'w') as f:
         writer = csv.writer(f)
         for line in data.iter_lines():
             writer.writerow(line.decode('utf-8').split(','))
+    print(f'Writing {filename} complete')
 
-ecdc_url = 'https://covid.ourworldindata.org/data/ecdc/full_data.csv'
+total_cases_url = 'https://covid.ourworldindata.org/data/ecdc/total_cases.csv'
+download_csv(total_cases_url, 'total_cases')
 
-download_csv(ecdc_url, 'ecdc')
+total_deaths_url = 'https://covid.ourworldindata.org/data/ecdc/total_deaths.csv'
+download_csv(total_deaths_url, 'total_deaths')
