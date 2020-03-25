edb2444a1c6de14e7539e9245a700ff687997301
spacebjorn <30187002+Spacebjorn@users.noreply.github.com>
2020-03-25 16:28:36 +0100

    changed script to output json in correct format

--- csv_to_json.py
+++ csv_to_json.py
@@ -1,19 +1,25 @@
 import os
 import pandas as pd
+import json
 
 json_dir = 'json'
-
-os.makedirs(json_dir, exist_ok=True)
-
 csv_dir = './csv'
 
 if not os.path.isdir(csv_dir):
     raise NameError('csv folder doesnt exist')
 
-for filename in os.listdir(csv_dir):
-    if filename.endswith('.csv'): 
-        csv_path = os.path.join(csv_dir, filename)
-        df = pd.read_csv(csv_path, index_col=0)
-        df = df.fillna(0)
-        filename = filename.split('.')[0]
-        df.to_json(f'./{json_dir}/{filename}.json',orient='columns')
+os.makedirs(json_dir, exist_ok=True)
+
+file_name = 'full_data.csv'
+csv_path = os.path.join(csv_dir, file_name)
+df = pd.read_csv(csv_path)
+df = df.rename(columns={"total_deaths": "deaths", "total_cases": "cases"})
+file_name = file_name.split('.')[0]
+data = {}
+for group in df.groupby(['location']):
+    data[group[0]] = { 
+            "cases": group[1][["date", "cases"]].values.tolist(),
+            "deaths": group[1][["date", "deaths"]].values.tolist(),
+        }
+
+open(f'./json/{file_name}.json', 'w').write(json.dumps(data))
