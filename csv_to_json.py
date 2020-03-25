dcf1d86efb08259d3468e15c3a8b1deafdd0479a
Sebastian Olsen <30187002+Spacebjorn@users.noreply.github.com>
2020-03-25 21:06:56 +0100

    Feature/json data format (#4)
    * second json data format iteration
    
    * fixed object key labels
    
    * changed script to output json in correct format
    
    * formating
    
    * combining cases and deaths into one dataset

--- csv_to_json.py
+++ csv_to_json.py
@@ -1,19 +1,38 @@
 import os
 import pandas as pd
+import json
 
 json_dir = 'json'
-
-os.makedirs(json_dir, exist_ok=True)
-
 csv_dir = './csv'
 
 if not os.path.isdir(csv_dir):
     raise NameError('csv folder doesnt exist')
 
-for filename in os.listdir(csv_dir):
-    if filename.endswith('.csv'): 
-        csv_path = os.path.join(csv_dir, filename)
-        df = pd.read_csv(csv_path, index_col=0)
-        df = df.fillna(0)
-        filename = filename.split('.')[0]
-        df.to_json(f'./{json_dir}/{filename}.json',orient='columns')
+os.makedirs(json_dir, exist_ok=True)
+
+
+cases_name = 'total_cases'
+cases_path = os.path.join(csv_dir, f'{cases_name}.csv')
+
+cases = pd.read_csv(cases_path, index_col=0)
+cases = cases.unstack().to_frame()
+cases.columns = ['cases']
+
+deaths_name = 'total_deaths'
+deaths_path = os.path.join(csv_dir, f'{deaths_name}.csv')
+deaths = pd.read_csv(deaths_path, index_col=0)
+deaths = deaths.unstack().to_frame()
+deaths.columns = ['deaths']
+
+df = cases.join(deaths)
+df = df.reset_index(level=[1])
+
+data = {}
+for group in df.groupby(level=0):
+    data[group[0]] = { 
+        'cases': group[1][['date', 'cases']].values.tolist(),
+        'deaths': group[1][['date', 'deaths']].values.tolist(),
+    }
+
+file_name = 'full_data'
+open(f'./json/{file_name}.json', 'w').write(json.dumps(data))
