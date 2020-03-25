ffea520f311454813b0a61b206f6730b67963ba8
spacebjorn <30187002+Spacebjorn@users.noreply.github.com>
2020-03-25 20:58:46 +0100

    combining cases and deaths into one dataset

--- csv_to_json.py
+++ csv_to_json.py
@@ -10,17 +10,29 @@ if not os.path.isdir(csv_dir):
 
 os.makedirs(json_dir, exist_ok=True)
 
-file_name = 'full_data'
 
-csv_path = os.path.join(csv_dir, f'{file_name}.csv')
-df = pd.read_csv(csv_path)
-df = df.rename(columns={"total_deaths": "deaths", "total_cases": "cases"})
+cases_name = 'total_cases'
+cases_path = os.path.join(csv_dir, f'{cases_name}.csv')
+
+cases = pd.read_csv(cases_path, index_col=0)
+cases = cases.unstack().to_frame()
+cases.columns = ['cases']
+
+deaths_name = 'total_deaths'
+deaths_path = os.path.join(csv_dir, f'{deaths_name}.csv')
+deaths = pd.read_csv(deaths_path, index_col=0)
+deaths = deaths.unstack().to_frame()
+deaths.columns = ['deaths']
+
+df = cases.join(deaths)
+df = df.reset_index(level=[1])
 
 data = {}
-for group in df.groupby(['location']):
+for group in df.groupby(level=0):
     data[group[0]] = { 
-            "cases": group[1][["date", "cases"]].values.tolist(),
-            "deaths": group[1][["date", "deaths"]].values.tolist(),
-        }
+        'cases': group[1][['date', 'cases']].values.tolist(),
+        'deaths': group[1][['date', 'deaths']].values.tolist(),
+    }
 
+file_name = 'full_data'
 open(f'./json/{file_name}.json', 'w').write(json.dumps(data))
