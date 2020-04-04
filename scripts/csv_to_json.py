e000289a5c88ddb168f4b999e61ec91820d1dd4f
Spacebjorn <30187002+Spacebjorn@users.noreply.github.com>
2020-04-04 12:31:31 +0200

    Added ISO alpha3 country codes to the json output

--- scripts/csv_to_json.py
+++ scripts/csv_to_json.py
@@ -37,13 +37,34 @@ new_deaths = get_csv_data('new_deaths', 'new_deaths')
 deaths = deaths.join(new_deaths)
 
 df = cases.join(deaths)
+df.index.names = ['Country', 'date']
 df['death_rate'] = df['deaths'] / df['cases'] * 100
 df = df.fillna(0)
+
+alpha3 = pd.read_csv('../data/country_codes.csv')
+pattern = {',': '','ç': 'c', 'ô': 'o', '’': "'", 'é': ''}
+alpha3['Country or Area'] = alpha3['Country or Area'].replace(pattern, regex=True)
+alpha3['Country or Area'] = alpha3['Country or Area'].str.replace('\(.*\)', '',regex=True)
+alpha3['Country or Area'] = alpha3['Country or Area'].str.replace('\s+the', '',regex=True)
+alpha3['Country or Area'] = alpha3['Country or Area'].str.strip()
+alpha3 = alpha3.set_index('Country or Area')
+alpha3 = alpha3['ISO-alpha3 code']
+
+country_fixes = {'World': 'NA', 'Brunei': 'BRN', 'Czech Republic': 'CZE', 'Vatican': 'VAT', 'United States': 'USA', 'United Kingdom': 'GBR', 'Faeroe Islands': 'FLK', 'International': 'NA', 'Timor': 'TLS', 'Tanzania': 'TZA', 'Kosovo': 'RKS','Taiwan': 'TWN', 'Laos': 'LAO', 'Syria': 'SYR', 'Macedonia': 'MKD', 'Swaziland': 'SWZ', 'Moldova': 'MDA', 'South Korea': 'KOR', 'Palestine': 'PSE', 'Russia': 'RUS', 'Sint Maarten (Dutch part)': 'SXM', 'Saint Vincent and the Grenadines': 'VCT'}
+country_fixes = pd.Series(country_fixes)
+alpha3 = alpha3.append(country_fixes)
+
+df['Country'] = df.index.get_level_values(0)
+df['alpha3'] = df['Country'].map(alpha3)
+df = df.drop(columns='Country')
 df = df.reset_index(level=[1])
+
+alpha3_dict = df['alpha3'].to_dict()
 
 data = {}
 for group in df.groupby(level=0):
-    data[group[0]] = { 
+    data[group[0]] = {
+        'alpha3': group[1]['alpha3'].unique()[0],
         'cases': group[1][['date', 'cases']].values.tolist(),
         'newCases': group[1][['date', 'new_cases']].values.tolist(),
         'casesPct': group[1][['date', 'percentage_cases']].values.tolist(),
