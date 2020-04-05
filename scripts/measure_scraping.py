a09fb8dc3d1a0f84789dba35994b5aa663f59748
Stef Brands <stef_brands@live.nl>
2020-04-05 19:03:43 +0200

    Add output to csv. No dates extracted.

--- scripts/measure_scraping.py
+++ scripts/measure_scraping.py
@@ -2,6 +2,7 @@ import requests
 from bs4 import BeautifulSoup
 import datefinder
 import pandas as pd
+import os.path as op
 wiki = "https://en.wikipedia.org/wiki/2019%E2%80%9320_coronavirus_pandemic_by_country_and_territory"
 
 country_url_dict = {
@@ -19,16 +20,15 @@ country_url_dict = {
     'Sweden': "https://en.wikipedia.org/wiki/2020_coronavirus_pandemic_in_Sweden",
     'Finland': "https://en.wikipedia.org/wiki/2020_coronavirus_pandemic_in_Finland",
     'United States': "https://en.wikipedia.org/wiki/2020_coronavirus_pandemic_in_the_United_States",
-    'Canada': "https://en.wikipedia.org/wiki/2020_coronavirus_pandemic_in_Canada"
+    'Canada': "https://en.wikipedia.org/wiki/2020_coronavirus_pandemic_in_Canada",
+    'India': "https://en.wikipedia.org/wiki/2020_coronavirus_pandemic_in_India"
 }
 
-df = pd.DataFrame()
-print(df)
-# country_events = {}
+country_events = {}
 for country in country_url_dict.items():
     print(country[0], country[1])
     request = requests.get(country[1])
-    soup = BeautifulSoup(request.text)
+    soup = BeautifulSoup(request.text, "html.parser")
     events = []
     timeline = soup.find(id='Timeline').parent
     # print(timeline)
@@ -39,8 +39,9 @@ for country in country_url_dict.items():
             continue
         # matches = list(datefinder.find_dates(elem.text))
         events.append(elem.text)
-    df[country[0]] = events
-    # country_events[country[0]] = events
+    country_events[country[0]] = events
 
+df = pd.DataFrame({k: pd.Series(v) for k, v in country_events.items()})
 # print(country_events)
 print(df)
+df.to_csv(op.join('..', 'output', 'covid_measures.csv'))
