7c3da5bf74c3583475519d1fd7726f0d6b2a0bad
Sebastian Olsen <30187002+Spacebjorn@users.noreply.github.com>
2020-04-07 21:11:29 +0200

    fixed json name (#19)
    * fixed json name

--- scripts/measures_to_json.py
+++ scripts/measures_to_json.py
@@ -6,4 +6,4 @@ data_dir = os.path.abspath(os.path.join(os.path.dirname( __file__ ), '..', 'data
 
 df = pd.read_csv(f'{data_dir}/containment_measures.csv')
 df = df.drop(columns=['ID', 'Applies To', 'Exceptions','Target state', 'Target region', 'Target country', 'Target city'])
-df.to_json(f'{json_dir}/measurements.json', force_ascii=False, orient='records')
+df.to_json(f'{json_dir}/containment_measures.json', force_ascii=False, orient='records')
