fb19d567855ca3de62e3d91c8b8399c52895c9e2
Spacebjorn <30187002+Spacebjorn@users.noreply.github.com>
2020-04-11 11:24:31 +0200

    Added TODOs

--- scripts/main.py
+++ scripts/main.py
@@ -1,3 +1,8 @@
 import download_data
 import ecdc_to_json
 import recovery_to_json
+
+
+# TODO Implement above imports as functions and call them from in here
+# TODO  create dir path in here and pass as function arguments to above script files
+# TODO Create a function in a separate file that does the alpha3 magic and pass DF as df / save file somewhere idc
